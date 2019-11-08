b45c62e7b5294129510d8ee106f3d0a1dd414569
Bruna Fistarol <30641224+brunafistarol@users.noreply.github.com>
2019-11-08 19:00:40 -0300

    questão 4 finalizada

--- questao-4.py
+++ questao-4.py
@@ -1,30 +1,71 @@
+
+# coding: utf-8
+
+# In[139]:
+
+
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+
+
+# In[140]:
+
 
 def questao4_1():
     s = 0
+    Xn = []
     Tn = []
     x = 0
     while s + x < 5:
         s += x
-        Tn.append(x)
+        Xn.append(x)
         x = np.random.exponential(1)
-    Tn.append(5-s) 
+        Tn.append(s)
+    Xn.append(5-s)
+    Tn.append(5)
     
     #print(Tn)
     
-    Nt =  0
-    for i in range(1, len(Tn)-1):
-        Nt += i*Tn[i]
+    It =  0
+    Nt = [0]
+    for i in range(1, len(Xn)-1):
+        Nt.append(i)
+        It += i*Xn[i]
         
-    return Nt
+    return Xn, Nt, Tn, It
 
-print ("item 1 :",questao4_1())
+
+# In[163]:
+
+
+Xn, Nt, Tn, It = questao4_1()
+
+
+# In[164]:
+
+
+Nt, Tn
+
+
+# In[36]:
+
+
+plt.step(Tn, Nt + [Nt[-1]], where='post')
+
+
+# In[40]:
+
 
 s=0
 for i in range(100000):
-    s+=questao4_1()
-print ("s/100000: ", s/100000)
+    _, _, _, It = questao4_1()
+    s += It
+s/100000
+
+
+# In[170]:
+
 
 def questao4_2():
     n = np.random.poisson(5)
@@ -33,16 +74,39 @@ def questao4_2():
         Tn.append(np.random.uniform(0., 5.))
     Tn.sort()
     Tn.append(5)
-    Nt = 0
+    It = 0
+    Nt = [0]
     for i in range(1, len(Tn)):
-        Nt += (i)*(Tn[i]-Tn[i-1])       
-    return Nt
+        It += (i)*(Tn[i]-Tn[i-1]) 
+        if i != len(Tn)-1:
+            Nt.append(i)
+    return It, Nt, Tn
 
 
-print ("item 2: ",questao4_2()) 
+# In[171]:
+
+
+In, Nt, Tn = questao4_2()
+
+
+# In[172]:
+
+
+Nt, Tn
+
+
+# In[173]:
+
+
+plt.step(Tn, Nt + [Nt[-1]], where='post')
+
+
+# In[174]:
+
 
 s=0
 for i in range(100000):
-    s+=questao4_2()
-print ("s/100000",s/100000)
+    It, _, _, = questao4_2()
+    s += It
+s/100000
 
