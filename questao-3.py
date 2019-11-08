1a5738de0249863aaed9e9d03ab2d579602ff1a3
Bruna Fistarol <30641224+brunafistarol@users.noreply.github.com>
2019-11-08 18:45:09 -0300

    Add files via upload
    questão 3 plot corrigido

--- questao-3.py
+++ questao-3.py
@@ -1,22 +1,32 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
 import random
 import matplotlib.pyplot as plt
 
 def martingal():
-    a = 0
-    m0 = 1
-    v = []
-    for i in range(100):
-        a = random.random()
-        if a > 0.5:
-            m0 = m0*(1/2)
-        else:
-            m0 = m0*(3/2)
-        v.append(m0)
-    plt.plot(v)
-    plt.axis([0, 100, 0, 0.5])
+    for j in range(10):
+        a = 0
+        m0 = 1
+        v = []
+        for i in range(100):
+            a = random.random()
+            if a > 0.5:
+                m0 = m0*(1/2)
+            else:
+                m0 = m0*(3/2)
+            v.append(m0)
+        plt.plot(v)
+        plt.axis([0, 100, 0, 0.5])
+        print(m0)
     plt.show()
-    print(m0)
 
-for i in range(1):
-    martingal()
+
+# In[5]:
+
+
+martingal()
 
