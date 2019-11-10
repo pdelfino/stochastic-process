5b6aa4a99c26861901c3055fc69121461b21a456
Pedro Delfino <p.delfino01@gmail.com>
2019-11-10 15:45:12 -0300

    resolver problema de conflito da questão 7

--- questao-7.py
+++ questao-7.py
@@ -1,21 +1,30 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# In[13]:
+
+
 def black_scholes(n): #n é o número de valores S gerados
     W = np.random.normal(size = n)
-    S = np.zeros(shape = n)
-    for i in range(n):
-        S[i] = 100*np.exp(-0.055+ W[i]*0.04)
+    S = 100*np.exp(-0.055 + W*0.04)
     K = np.linspace(80, 120, 9)
     C = np.zeros(shape = 9)
     for k in range(9):
-        for i in S:
-            if i > K[k]:
-                C[k] += np.exp(-0.05)*(i - K[k])
-        C[k] = C[k]/n
-        
-    plt.plot(K, C, 'bo')
+        C[k] = np.mean(np.exp(-0.05) * np.maximum(S - K[k], 0.0))
+    
+    plt.plot(K, C)
     plt.show
 
-print(black_scholes(100))
+
+# In[14]:
+
+
+black_scholes(100)
 
