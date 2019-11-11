3935e8f95091953fde4e6e82deab384e8452ca49
Pedro Delfino <p.delfino01@gmail.com>
2019-11-11 11:09:08 -0300

    refatorar questao-5

--- questao-5.py
+++ questao-5.py
@@ -1,45 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def kernel_f(x,xl,sigma,l):
+#xl é o x linha
+# exemplo 6.2.2 da apostila
+def nucleo(x,xl,sigma,l):
+    
     return sigma**2*np.exp(-5.*(1/l**2)*(x-xl)**2)
 
-def K(X,Xl,sigma,l):
-    n, m = X.shape[0],Xl.shape[0]
+
+# calculando a matriz do Kzão
+def matrix_K(X,Xl,sigma,l):
+    
+    n = X.shape[0]
+    
+    m = Xl.shape[0]
+    
     K = np.zeros((n,m))
+    
     for i in range(n):
+        
         for j in range(m):
-            K[i,j] = kernel_f(X[i], Xl[j], sigma, l)
+            
+            K[i,j] = nucleo(X[i], Xl[j], sigma, l)
+    
     return K
 
-def exercicio5(X, X_new, Y, sigma):
-    KXX = K(X,X,1,1)
-    KXnX = K(X_new, X, 1, 1)
-    KXXn = K(X,X_new, 1, 1)
-    KXnXn = K(X_new, X_new, 1, 1)
+#retorna a média e a covarianceariância, pag 95 da apostila
+def media_covariancear(X, X_new, Y, sigma):
+    KXX = matrix_K(X,X,1,1)
+    KXnX = matrix_K(X_new, X, 1, 1)
+    KXXn = matrix_K(X,X_new, 1, 1)
+    KXnXn = matrix_K(X_new, X_new, 1, 1)
 
     aux = KXnX.dot(np.linalg.inv(KXX+sigma**2*np.identity(KXX.shape[0])))
 
-    mean = aux.dot(Y)
+    media = aux.dot(Y)
     covariance = KXnXn + sigma**2*np.identity(KXnXn.shape[0]) - aux.dot(KXXn)
 
-    return (mean,covariance)
+    return (media,covariance)
 
-n, m = 9, 1000
+n = 9
+m = 1000
 
 X = np.linspace(-5,5,n)
 Y = np.sin(X)+np.random.normal(loc = 0, scale = 0.001, size=n)
 Xn = np.linspace(-5,5,m)
 
-mu, cov = exercicio5(X, Xn, Y, 0.01)
+media = media_covariancear(X, Xn, Y, 0.01)[0]
+covariance = media_covariancear(X, Xn, Y, 0.01)[1]
 
-stdv = np.sqrt(np.diag(cov))
+stdv = np.sqrt(np.diag(covariance))
 
-plt.plot(X,Y, 'bs', ms=8, label="Pontos iniciais")
-plt.plot(Xn,mu,label="Média")
-plt.gca().fill_between(Xn, mu-2*stdv,mu+2*stdv,color="#dddddd",label="intervalo de confiança")
-plt.plot(Xn,mu,"y--",lw=2,label="função aproximada")
+plt.plot(X,Y, 'bs', ms=8, label="Initial points")
+
+plt.plot(Xn,media,label="Mean")
+
+plt.gca().fill_between(Xn, media-2*stdv,media+2*stdv,color="#dddddd",label="confidence interval")
+
+plt.plot(Xn,media,"y--",lw=2,label="approximate function")
+
 plt.axis([-5,5,-3,3])
+
 plt.legend()
+
 plt.show()
 
